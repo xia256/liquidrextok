a53e5dfdfa23659d81ecede2fc349876335267ac
xia256 <asphyxia@novusphere.io>
2022-04-19 19:16:28 -0400

    initial commit

---
+++ liquidrextok.cpp
@@ -0,0 +1,277 @@
+#include "liquidrextok.hpp"
+
+void liquidrextok::create(const name &issuer,
+						  const asset &maximum_supply)
+{
+	require_auth(get_self());
+
+	auto sym = maximum_supply.symbol;
+	check(sym.is_valid(), "invalid symbol name");
+	check(maximum_supply.is_valid(), "invalid supply");
+	// check(maximum_supply.amount > 0, "max-supply must be positive");
+
+	stats statstable(get_self(), sym.code().raw());
+	auto existing = statstable.find(sym.code().raw());
+	check(existing == statstable.end(), "token with symbol already exists");
+
+	statstable.emplace(get_self(), [&](auto &s)
+					   {
+       s.supply.symbol = maximum_supply.symbol;
+       s.max_supply    = maximum_supply;
+       s.issuer        = issuer; });
+}
+
+void liquidrextok::issue(const name &to, const asset &quantity, const string &memo)
+{
+	auto sym = quantity.symbol;
+	check(sym.is_valid(), "invalid symbol name");
+	check(memo.size() <= 256, "memo has more than 256 bytes");
+
+	stats statstable(get_self(), sym.code().raw());
+	auto existing = statstable.find(sym.code().raw());
+	check(existing != statstable.end(), "token with symbol does not exist, create token before issue");
+	const auto &st = *existing;
+	check(to == st.issuer, "tokens can only be issued to issuer account");
+
+	require_auth(st.issuer);
+	check(quantity.is_valid(), "invalid quantity");
+	check(quantity.amount > 0, "must issue positive quantity");
+
+	check(quantity.symbol == st.supply.symbol, "symbol precision mismatch");
+	// check(quantity.amount <= st.max_supply.amount - st.supply.amount, "quantity exceeds available supply");
+
+	statstable.modify(st, same_payer, [&](auto &s)
+					  { s.supply += quantity; });
+
+	add_balance(st.issuer, quantity, st.issuer);
+}
+
+void liquidrextok::retire(const asset &quantity, const string &memo)
+{
+	auto sym = quantity.symbol;
+	check(sym.is_valid(), "invalid symbol name");
+	check(memo.size() <= 256, "memo has more than 256 bytes");
+
+	stats statstable(get_self(), sym.code().raw());
+	auto existing = statstable.find(sym.code().raw());
+	check(existing != statstable.end(), "token with symbol does not exist");
+	const auto &st = *existing;
+
+	require_auth(st.issuer);
+	check(quantity.is_valid(), "invalid quantity");
+	check(quantity.amount > 0, "must retire positive quantity");
+
+	check(quantity.symbol == st.supply.symbol, "symbol precision mismatch");
+
+	statstable.modify(st, same_payer, [&](auto &s)
+					  { s.supply -= quantity; });
+
+	//sub_balance(st.issuer, quantity);
+}
+
+void liquidrextok::transfer(const name from,
+							const name to,
+							const asset quantity,
+							const string memo)
+{
+	check(from != to, "cannot transfer to self");
+	require_auth(from);
+	check(is_account(to), "to account does not exist");
+	auto sym = quantity.symbol.code();
+	stats statstable(get_self(), sym.raw());
+	const auto &st = statstable.get(sym.raw());
+
+	require_recipient(from);
+	require_recipient(to);
+
+	check(quantity.is_valid(), "invalid quantity");
+	check(quantity.amount > 0, "must transfer positive quantity");
+	check(quantity.symbol == st.supply.symbol, "symbol precision mismatch");
+	// check(memo.size() <= 256, "memo has more than 256 bytes");
+
+	auto payer = has_auth(to) ? to : from;
+
+	sub_balance(from, quantity);
+
+	if (to == get_self())
+	{
+		liquidrextok::retire_action retire(_self, {get_self(), "active"_n});
+		liquidrextok::rex_sellrex_action sellrex(eosio::name("eosio"), {get_self(), "active"_n});
+		liquidrextok::redeemrex_action redeemrex(_self, {get_self(), "active"_n});
+
+		retire.send(quantity, "redeem REX tokens");
+		sellrex.send(get_self(), quantity);
+		redeemrex.send(from);
+	}
+	else
+	{
+		add_balance(to, quantity, payer);
+	}
+}
+
+void liquidrextok::sub_balance(const name &owner, const asset &value)
+{
+	accounts from_acnts(get_self(), owner.value);
+
+	const auto &from = from_acnts.get(value.symbol.code().raw(), "no balance object found");
+	check(from.balance.amount >= value.amount, "overdrawn balance (liquidrextok)");
+
+	from_acnts.modify(from, owner, [&](auto &a)
+					  { a.balance -= value; });
+}
+
+void liquidrextok::add_balance(const name &owner, const asset &value, const name &ram_payer)
+{
+	accounts to_acnts(get_self(), owner.value);
+	auto to = to_acnts.find(value.symbol.code().raw());
+	if (to == to_acnts.end())
+	{
+		to_acnts.emplace(ram_payer, [&](auto &a)
+						 { a.balance = value; });
+	}
+	else
+	{
+		to_acnts.modify(to, same_payer, [&](auto &a)
+						{ a.balance += value; });
+	}
+}
+
+void liquidrextok::open(const name &owner, const symbol &symbol, const name &ram_payer)
+{
+	require_auth(ram_payer);
+
+	check(is_account(owner), "owner account does not exist");
+
+	auto sym_code_raw = symbol.code().raw();
+	stats statstable(get_self(), sym_code_raw);
+	const auto &st = statstable.get(sym_code_raw, "symbol does not exist");
+	check(st.supply.symbol == symbol, "symbol precision mismatch");
+
+	accounts acnts(get_self(), owner.value);
+	auto it = acnts.find(sym_code_raw);
+	if (it == acnts.end())
+	{
+		acnts.emplace(ram_payer, [&](auto &a)
+					  { a.balance = asset{0, symbol}; });
+	}
+}
+
+void liquidrextok::close(const name &owner, const symbol &symbol)
+{
+	require_auth(owner);
+	accounts acnts(get_self(), owner.value);
+	auto it = acnts.find(symbol.code().raw());
+	check(it != acnts.end(), "Balance row already deleted or never existed. Action won't have any effect.");
+	check(it->balance.amount == 0, "Cannot close because the balance is not zero.");
+	acnts.erase(it);
+}
+
+void liquidrextok::test(const name &from)
+{
+
+}
+
+int64_t liquidrextok::get_core_balance()
+{
+	accounts accs(eosio::name("eosio.token"), _self.value);
+
+	int64_t balance = 0;
+	auto it = accs.find(eosio::symbol("TLOS", 4).code().raw());
+	if (it != accs.end())
+	{
+		balance = it->balance.amount;
+	}
+
+	return balance;
+}
+
+int64_t liquidrextok::get_rex_balance()
+{
+	liquidrextok::rex_balance_table rexbalance(eosio::name("eosio"), eosio::name("eosio").value);
+	
+	int64_t balance = 0;
+	auto it = rexbalance.find(_self.value);
+	if (it != rexbalance.end())
+	{
+		balance = it->rex_balance.amount;
+	}
+	
+	return balance;
+}
+
+void liquidrextok::issuerex(name from, name to, asset quantity, string memo)
+{
+	if (from == _self)
+		return; // sending tokens, ignore
+
+	if (from == eosio::name("eosio.rex"))
+		return; // ignore redeeming TLOS from rex
+
+	check(to == _self, "stop trying to hack the contract");
+	check(quantity.amount > 0, "quantity amount must be greater than zero");
+
+	int64_t balance = this->get_rex_balance();
+
+	liquidrextok::rex_deposit_action deposit(eosio::name("eosio"), {get_self(), "active"_n});
+	liquidrextok::rex_buyrex_action buyrex(eosio::name("eosio"), {get_self(), "active"_n});
+	liquidrextok::issuerex2_action issuerex2(_self, {get_self(), "active"_n});
+
+	deposit.send(get_self(), quantity);
+	buyrex.send(get_self(), quantity);
+	issuerex2.send(from, balance);
+}
+
+void liquidrextok::issuerex2(name recipient, int64_t rex_balance)
+{
+	require_auth(_self);
+
+	int64_t balance = this->get_rex_balance();
+
+	check(balance > rex_balance, "rex balance did not increase");
+
+	eosio::asset quantity(balance - rex_balance, eosio::symbol("REX", 4));
+
+	liquidrextok::issue_action issue(_self, {get_self(), "active"_n});
+	liquidrextok::transfer_action transfer(_self, {get_self(), "active"_n});
+
+	issue.send(get_self(), quantity, "mint new tokens");
+	transfer.send(get_self(), recipient, quantity, "transfer new tokens to recipient");
+}
+
+void liquidrextok::redeemrex(name recipient) 
+{
+	require_auth(_self);
+
+	int64_t balance = this->get_core_balance();
+
+	liquidrextok::rex_fund_table rexfund(eosio::name("eosio"), eosio::name("eosio").value);
+	auto it = rexfund.find(_self.value);
+	check(it != rexfund.end(), "no rexfund found");
+
+	liquidrextok::rex_withdraw_action withdraw(eosio::name("eosio"), {get_self(), "active"_n});
+	liquidrextok::transfer_action transfer(eosio::name("eosio.token"), {get_self(), "active"_n});
+
+	withdraw.send(get_self(), it->balance);
+	transfer.send(get_self(), recipient, it->balance, "redeem REX tokens");
+}
+
+extern "C" void apply(uint64_t receiver, uint64_t code, uint64_t action)
+{
+	if (code != receiver)
+	{
+		if (code == eosio::name("eosio.token").value && action == eosio::name("transfer").value)
+		{
+			eosio::execute_action(eosio::name(receiver), eosio::name(code), &liquidrextok::issuerex);
+		}
+	}
+	else if (code == receiver)
+	{
+		//
+		// Self dispatched actions (callable contract methods)
+		//
+		switch (action)
+		{
+			EOSIO_DISPATCH_HELPER(liquidrextok, (create)(issue)(retire)(transfer)(open)(close)(test)(issuerex2)(redeemrex));
+		}
+	}
+}
---
+++ liquidrextok.hpp
@@ -0,0 +1,204 @@
+#include <eosio/eosio.hpp>
+#include <eosio/asset.hpp>
+#include <eosio/time.hpp>
+#include <eosio/crypto.hpp>
+#include <eosio/system.hpp>
+
+#include <string>
+#include <vector>
+#include <memory>
+
+using namespace std;
+using namespace eosio;
+
+CONTRACT liquidrextok : public contract
+{
+public:
+    using eosio::contract::contract;
+
+    /**
+     * Allows `issuer` account to create a token in supply of `maximum_supply`. If validation is successful a new entry in statstable for token symbol scope gets created.
+     *
+     * @param issuer - the account that creates the token,
+     * @param maximum_supply - the maximum supply set for the token created.
+     *
+     * @pre Token symbol has to be valid,
+     * @pre Token symbol must not be already created,
+     * @pre maximum_supply has to be smaller than the maximum supply allowed by the system: 1^62 - 1.
+     * @pre Maximum supply must be positive;
+     */
+    ACTION create(const name &issuer,
+                  const asset &maximum_supply);
+    /**
+     *  This action issues to `to` account a `quantity` of tokens.
+     *
+     * @param to - the account to issue tokens to, it must be the same as the issuer,
+     * @param quntity - the amount of tokens to be issued,
+     * @memo - the memo string that accompanies the token issue transaction.
+     */
+    ACTION issue(const name &to, const asset &quantity, const string &memo);
+
+    /**
+     * The opposite for create action, if all validations succeed,
+     * it debits the statstable.supply amount.
+     *
+     * @param quantity - the quantity of tokens to retire,
+     * @param memo - the memo string to accompany the transaction.
+     */
+    ACTION retire(const asset &quantity, const string &memo);
+
+    /**
+     * Allows `from` account to transfer to `to` account the `quantity` tokens.
+     * One account is debited and the other is credited with quantity tokens.
+     *
+     * @param from - the account to transfer from,
+     * @param to - the account to be transferred to,
+     * @param quantity - the quantity of tokens to be transferred,
+     * @param memo - the memo string to accompany the transaction.
+     */
+    ACTION transfer(const name from,
+                    const name to,
+                    const asset quantity,
+                    const string memo);
+    /**
+     * Allows `ram_payer` to create an account `owner` with zero balance for
+     * token `symbol` at the expense of `ram_payer`.
+     *
+     * @param owner - the account to be created,
+     * @param symbol - the token to be payed with by `ram_payer`,
+     * @param ram_payer - the account that supports the cost of this action.
+     *
+     * More information can be read [here](https://github.com/EOSIO/eosio.contracts/issues/62)
+     * and [here](https://github.com/EOSIO/eosio.contracts/issues/61).
+     */
+    ACTION open(const name &owner, const symbol &symbol, const name &ram_payer);
+
+    /**
+     * This action is the opposite for open, it closes the account `owner`
+     * for token `symbol`.
+     *
+     * @param owner - the owner account to execute the close action for,
+     * @param symbol - the symbol of the token to execute the close action for.
+     *
+     * @pre The pair of owner plus symbol has to exist otherwise no action is executed,
+     * @pre If the pair of owner plus symbol exists, the balance has to be zero.
+     */
+    ACTION close(const name &owner, const symbol &symbol);
+
+    ACTION test(const name &from);
+
+    // Public but not a directly callable action
+    // Called indirectly by sending TLOS to this contract
+    ACTION issuerex(name from, name to, asset quantity, string memo);
+    ACTION issuerex2(name recipient, int64_t rex_balance);
+    ACTION redeemrex(name recipient);
+
+    // Dummy functions for action wrapper templates
+    ACTION log(string message) {}
+    ACTION dummydeposit(const name &owner, const asset &amount) {}
+    ACTION dummywithdrw(const name &owner, const asset &amount) {}
+    ACTION dummybuyrex(const name &from, const asset &amount) {}
+    ACTION dummysellrex(const name &from, const asset &amount) {}
+
+    static asset get_supply(const name &token_contract_account, const symbol_code &sym_code)
+    {
+        stats statstable(token_contract_account, sym_code.raw());
+        const auto &st = statstable.get(sym_code.raw());
+        return st.supply;
+    }
+
+    static asset get_balance(const name &token_contract_account, const name &owner, const symbol_code &sym_code)
+    {
+        accounts accountstable(token_contract_account, owner.value);
+        const auto &ac = accountstable.get(sym_code.raw());
+        return ac.balance;
+    }
+
+    using create_action = eosio::action_wrapper<"create"_n, &liquidrextok::create>;
+    using issue_action = eosio::action_wrapper<"issue"_n, &liquidrextok::issue>;
+    using retire_action = eosio::action_wrapper<"retire"_n, &liquidrextok::retire>;
+    using transfer_action = eosio::action_wrapper<"transfer"_n, &liquidrextok::transfer>;
+    using open_action = eosio::action_wrapper<"open"_n, &liquidrextok::open>;
+    using close_action = eosio::action_wrapper<"close"_n, &liquidrextok::close>;
+    using log_action = eosio::action_wrapper<"log"_n, &liquidrextok::log>;
+
+    // Interal Use
+    using issuerex2_action = eosio::action_wrapper<"issuerex2"_n, &liquidrextok::issuerex2>;
+    using redeemrex_action = eosio::action_wrapper<"redeemrex"_n, &liquidrextok::redeemrex>;
+
+    // REX
+    using rex_deposit_action = eosio::action_wrapper<"deposit"_n, &liquidrextok::dummydeposit>;
+    using rex_withdraw_action = eosio::action_wrapper<"withdraw"_n, &liquidrextok::dummywithdrw>;
+    using rex_buyrex_action = eosio::action_wrapper<"buyrex"_n, &liquidrextok::dummybuyrex>;
+    using rex_sellrex_action = eosio::action_wrapper<"sellrex"_n, &liquidrextok::dummysellrex>;
+
+private:
+    struct [[eosio::table]] account
+    {
+        asset balance;
+
+        uint64_t primary_key() const { return balance.symbol.code().raw(); }
+    };
+
+    struct [[eosio::table]] currency_stats
+    {
+        asset supply;
+        asset max_supply;
+        name issuer;
+
+        uint64_t primary_key() const { return supply.symbol.code().raw(); }
+    };
+
+    // https://github.com/telosnetwork/telos.contracts/blob/master/contracts/eosio.system/include/eosio.system/eosio.system.hpp#L473
+    /**
+     * `rex_balance` structure underlying the rex balance table.
+     *
+     * @details A rex balance table entry is defined by:
+     * - `version` defaulted to zero,
+     * - `owner` the owner of the rex fund,
+     * - `vote_stake` the amount of CORE_SYMBOL currently included in owner's vote,
+     * - `rex_balance` the amount of REX owned by owner,
+     * - `matured_rex` matured REX available for selling.
+     */
+    struct [[eosio::table, eosio::contract("eosio.system")]] rex_balance
+    {
+        uint8_t version = 0;
+        name owner;
+        asset vote_stake;
+        asset rex_balance;
+        int64_t matured_rex = 0;
+        std::deque<std::pair<time_point_sec, int64_t>> rex_maturities; /// REX daily maturity buckets
+
+        uint64_t primary_key() const { return owner.value; }
+    };
+
+    // `rex_fund` structure underlying the rex fund table. A rex fund table entry is defined by:
+    // - `version` defaulted to zero,
+    // - `owner` the owner of the rex fund,
+    // - `balance` the balance of the fund.
+    struct [[eosio::table, eosio::contract("eosio.system")]] rex_fund
+    {
+        uint8_t version = 0;
+        name owner;
+        asset balance;
+
+        uint64_t primary_key() const { return owner.value; }
+    };
+
+    /**
+     * Rex balance table
+     *
+     * @details The rex balance table is storing all the `rex_balance`s instances.
+     */
+    typedef eosio::multi_index<"rexbal"_n, rex_balance> rex_balance_table;
+    typedef eosio::multi_index<"rexfund"_n, rex_fund> rex_fund_table;
+
+    typedef eosio::multi_index<"accounts"_n, account> accounts;
+    typedef eosio::multi_index<"stat"_n, currency_stats> stats;
+
+    void sub_balance(const name &owner, const asset &value);
+    void add_balance(const name &owner, const asset &value, const name &ram_payer);
+
+    int64_t get_rex_balance();
+    int64_t get_core_balance();
+};
